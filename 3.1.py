6bf13e573a12a5545fe0b5c7da4c140a6a5bcee6
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2023-01-18 17:09:54 +0900

    3.1

---
+++ 3.1.py
@@ -0,0 +1,16 @@
+password_correct = False
+
+if password_correct:
+    print("Here is your money")
+else:
+    print("Wrong password")
+
+winner = 10
+
+if winner > 10:
+    print("winner is greater than 10")
+elif winner < 10:
+    print("winner is lesst than 10")
+else:
+    print("winner is 10")
+    
