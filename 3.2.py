522d30183901e056dae94efcb6dd8da04c2e6bd0
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2023-01-18 18:29:06 +0900

    3.2

---
+++ 3.2.py
@@ -0,0 +1,12 @@
+winner = 50
+
+if winner != 10:
+    print("If")
+elif winner <= 25:
+    print("Elif")
+elif winner == 0:
+    print("Elif 2")
+elif winner == 50:
+    print("elif3")
+else:
+    print("Else")
